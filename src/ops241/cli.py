ae055215020ce4144757356dabd1227da81ce3b3
Thys Meintjes <sthysel@gmail.com>
2019-03-23 16:35:23 +0800

    cli reader tool

--- src/ops241/cli.py
+++ src/ops241/cli.py
@@ -6,17 +6,32 @@ import click
 @click.command(context_settings=dict(max_content_width=120))
 @click.version_option()
 @click.option(
-    '--fps',
-    help='Frames per second rate for output file',
-    default=12.5,
+    '-p',
+    '--port',
+    help='TTY Port radar is available at',
+    default='/dev/ttyACM0',
     show_default=True,
 )
-def cli():
+@click.option(
+    '-j',
+    '--json-format',
+    help='JSON Output format',
+    default=True,
+    show_default=True,
+)
+@click.option(
+    '-m',
+    '--metric',
+    help='Use metric units',
+    default=True,
+    show_default=True,
+)
+def cli(port, json_format, metric):
     """
-    OPS241 Radar command tool
+    OPS241 Radar reader 
     """
 
-    with OPS241Radar() as radar:
+    with OPS241Radar(port=port, json_format=json_format, metric=metric) as radar:
         print(radar.get_module_information())
         while True:
             data = radar.read()
